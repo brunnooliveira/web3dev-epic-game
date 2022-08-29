03a462561eca4b5aba657e0cb6955b17047c0c43
brunnooliveira <brunnogoncalves@gmail.com>
2022-08-28 21:05:18 -0300

    feat(contract): define character attibutes

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,15 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    ["Paulinho Gogó", "Tirulipa", "João Plenário"],
+		[
+			"https://i.imgur.com/NhVLp0U.jpg",
+			"https://i.imgur.com/YzjG4Ie.jpg",
+			"https://i.imgur.com/vvcbDYY.jpg",
+		],
+    [100, 200, 300], // HP values
+    [100, 50, 25] // Attack damage values
+  );
   await gameContract.deployed();
   console.log("Contrato implantado no endereço:", gameContract.address);
 };
