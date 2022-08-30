f51ce78be39002d1f45c0f1d2b8515278de033e9
brunnooliveira <brunnogoncalves@gmail.com>
2022-08-29 23:22:53 -0300

    chore: deploy to testnet

--- README.md
+++ README.md
@@ -13,3 +13,26 @@ npx hardhat node
 node scripts/sample-script.js
 npx hardhat help
 ```
+
+## Deploy RINKEBY
+```bash
+Contrato implantado no endereço: 0x57Ac1dc8bEdEEe9b2BdCAadc0ee3980c2703A1F9
+Mintou NFT #1
+Mintou NFT #2
+Mintou NFT #3
+Minted NFT #4
+Fim do deploy e mint!
+```
+https://testnets.opensea.io/collection/comedians
+
+
+## Deploy GOERLI
+```bash
+Contrato implantado no endereço: 0xb239D8B4f868a5b0C263C52cB6cc0CBC5c1cddae
+Mintou NFT #1
+Mintou NFT #2
+Mintou NFT #3
+Minted NFT #4
+Fim do deploy e mint!
+```
+
---
+++ scripts/deploy.js
@@ -0,0 +1,46 @@
+const main = async () => {
+  const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
+  const gameContract = await gameContractFactory.deploy(
+    ["Paulinho Gogó", "Tirulipa", "João Plenário"],
+		[
+			"https://i.imgur.com/NhVLp0U.jpg",
+			"https://i.imgur.com/YzjG4Ie.jpg",
+			"https://i.imgur.com/vvcbDYY.jpg",
+		],
+    [100, 200, 300], // HP values
+    [100, 50, 25] // Attack damage values
+  );
+  await gameContract.deployed();
+  console.log("Contrato implantado no endereço:", gameContract.address)
+
+  let txn;
+  txn = await gameContract.mintCharacterNFT(0);
+  await txn.wait();
+  console.log("Mintou NFT #1");
+
+  txn = await gameContract.mintCharacterNFT(1);
+  await txn.wait();
+  console.log("Mintou NFT #2");
+
+  txn = await gameContract.mintCharacterNFT(2);
+  await txn.wait();
+  console.log("Mintou NFT #3");
+
+  txn = await gameContract.mintCharacterNFT(1);
+  await txn.wait();
+  console.log("Minted NFT #4");
+
+  console.log("Fim do deploy e mint!");
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
