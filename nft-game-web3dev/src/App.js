93905f5bd74592e011755a99c3fa7aef670feb41
brunnooliveira <brunnogoncalves@gmail.com>
2022-09-04 10:21:13 -0300

    feat: check wallet connected

--- nft-game-web3dev/src/App.js
+++ nft-game-web3dev/src/App.js
@@ -1,23 +1,86 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
 import twitterLogo from "./assets/twitter-logo.svg"
 import "./App.css"
 
 // Constants
-const TWITTER_HANDLE = "web3dev_"
+const TWITTER_HANDLE = "brunnogoliveira"
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
 
 const App = () => {
+
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Eu acho que você não tem a metamask!");
+        return;
+      } else {
+        console.log("Nós temos o objeto ethereum", ethereum);
+      }
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log("Carteira conectada::", account);
+          setCurrentAccount(account);
+        } else {
+          console.log("Não encontramos uma carteira conectada");
+        }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Instale a MetaMask!");
+        return;
+      }
+
+      /*
+       * Método chique para pedir acesso para a conta.
+       */
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      /*
+       * Boom! Isso deve escrever o endereço público uma vez que autorizarmos Metamask.
+       */
+      console.log("Contectado", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">⚔️ Batalhas no Metaverso ⚔️</p>
-          <p className="sub-text">Junte-se a mim para vencer os inimigos do Metaverso!</p>
+          <p className="header gradient-text">Comedians Battle</p>
+          <p className="sub-text">Vamos matar o Carlos Alberto de rir!!!</p>
           <div className="connect-wallet-container">
             <img
-              src="https://thumbs.gfycat.com/AnchoredPleasedBergerpicard-size_restricted.gif"
+              src="https://c.tenor.com/B3RKN_3MmtsAAAAC/lol-cazalbe.gif"
               alt="Nascimento Gif"
             />
+            <button
+              className="cta-button connect-wallet-button"
+              onClick={connectWalletAction}
+            >
+              Conecte sua carteira para começar
+            </button>
           </div>
         </div>
         <div className="footer-container">
