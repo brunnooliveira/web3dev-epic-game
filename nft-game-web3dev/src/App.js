96af5d7fd1eff43feb336da6a092392f3fd75fb4
brunnooliveira <brunnogoncalves@gmail.com>
2022-09-04 10:47:00 -0300

    feat: selectCharacter component

--- nft-game-web3dev/src/App.js
+++ nft-game-web3dev/src/App.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import twitterLogo from "./assets/twitter-logo.svg"
 import "./App.css"
+import SelectCharacter from "./Components/SelectCharacter";
+import { CONTRACT_ADDRESS } from "./constants";
+import myEpicGame from "./utils/MyEpicGame.json";
 
 // Constants
 const TWITTER_HANDLE = "brunnogoliveira"
@@ -9,6 +12,7 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
 const App = () => {
 
   const [currentAccount, setCurrentAccount] = useState(null);
+  const [characterNFT, setCharacterNFT] = useState(null);
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -47,20 +51,35 @@ const App = () => {
         return;
       }
 
-      /*
-       * Método chique para pedir acesso para a conta.
-       */
       const accounts = await ethereum.request({
         method: "eth_requestAccounts",
       });
 
-      /*
-       * Boom! Isso deve escrever o endereço público uma vez que autorizarmos Metamask.
-       */
       console.log("Contectado", accounts[0]);
       setCurrentAccount(accounts[0]);
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  const renderContent = () => {
+    if (!currentAccount) {
+      return (
+        <div className="connect-wallet-container">
+          <img
+            src="https://c.tenor.com/B3RKN_3MmtsAAAAC/lol-cazalbe.gif"
+            alt="Carlos Alberto"
+          />
+          <button
+            className="cta-button connect-wallet-button"
+            onClick={connectWalletAction}
+          >
+            Conecte sua carteira para começar
+          </button>
+        </div>
+      );
+    } else if (currentAccount && !characterNFT) {
+      return <SelectCharacter setCharacterNFT={setCharacterNFT} />;
     }
   };
 
@@ -70,18 +89,7 @@ const App = () => {
         <div className="header-container">
           <p className="header gradient-text">Comedians Battle</p>
           <p className="sub-text">Vamos matar o Carlos Alberto de rir!!!</p>
-          <div className="connect-wallet-container">
-            <img
-              src="https://c.tenor.com/B3RKN_3MmtsAAAAC/lol-cazalbe.gif"
-              alt="Nascimento Gif"
-            />
-            <button
-              className="cta-button connect-wallet-button"
-              onClick={connectWalletAction}
-            >
-              Conecte sua carteira para começar
-            </button>
-          </div>
+          {renderContent()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
